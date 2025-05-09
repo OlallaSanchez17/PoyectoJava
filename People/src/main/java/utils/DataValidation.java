5914efdeec5a3a4d6e9a6e13b6872eab407b982c
Olalla <olallasanchezbernad08@gmail.com>
2025-05-09 11:52:04 +0200

    #16 Reemplazar cadenas codificadas con constantes en la clase Constantes

--- People/src/main/java/utils/DataValidation.java
+++ People/src/main/java/utils/DataValidation.java
@@ -26,5 +26,13 @@ public class DataValidation {
             "N","J","Z","S","Q","V","H","L","C","K","E"};
         return nifNoLetter + letter[Integer.parseInt(nifNoLetter)%23];
     }
-
+    
+    public class StorageConstants {
+    public static final String ARRAY_LIST = "ArrayList";
+    public static final String HASH_MAP = "HashMap";
+    public static final String FILE = "File";
+    public static final String FILE_SERIALIZATION = "File (Serialization)";
+    public static final String SQL_DATABASE = "SQL - Database";
+    public static final String JPA_DATABASE = "JPA - Database";
+}
 }
